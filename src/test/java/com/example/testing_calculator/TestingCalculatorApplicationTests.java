17a78678ed997b9c6e6cf090ce6882cfdf8a452a
AnastasiaSergeeva05 <gotsenko_nasty@mail.ru>
2022-11-22 12:41:12 +0300

    Initial commit

---
+++ src/test/java/com/example/testing_calculator/TestingCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testing_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
